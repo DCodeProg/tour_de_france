6c4b0ce30c9e7f138c00106efa9a459cf603a880
DCodeProg <danael.legrand@outlook.fr>
2025-04-07 00:38:18 +0200

    feat: add functional programming usecase class and default errors

---
+++ lib/core/usecase/usecase.dart
@@ -0,0 +1,8 @@
+import 'package:tour_de_france/core/errors/failures.dart';
+import 'package:fpdart/fpdart.dart';
+
+abstract interface class UseCase<SuccessType, Params> {
+  Future<Either<Failure, SuccessType>> call(Params params);
+}
+
+class NoParams {}
