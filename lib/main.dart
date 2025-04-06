3e3cacddb1f33da6f0afcc6b52a782308602e8db
DCodeProg <danael.legrand@outlook.fr>
2025-04-07 00:33:03 +0200

    feat: add app theme with cubit

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:tour_de_france/core/secrets/app_secrets.dart';
+import 'package:tour_de_france/core/shared/cubit/theme_cubit.dart';
 
 import 'core/theme/app_theme.dart';
 import 'core/theme/text_theme.dart';
@@ -10,6 +15,14 @@ import 'core/theme/text_theme.dart';
 Future<void> main() async {
   WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
   FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+
+  HydratedBloc.storage = await HydratedStorage.build(
+    storageDirectory:
+        kIsWeb
+            ? HydratedStorageDirectory.web
+            : HydratedStorageDirectory((await getTemporaryDirectory()).path),
+  );
+
   await dotenv.load(fileName: '.env');
 
   await Supabase.initialize(
@@ -35,11 +48,21 @@ class MainApp extends StatelessWidget {
     );
     MaterialTheme theme = MaterialTheme(textTheme);
 
-    return MaterialApp(
-      title: "Tour De France",
-      debugShowCheckedModeBanner: false,
-      theme: theme.light(),
-      darkTheme: theme.dark(),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (context) => ThemeCubit()),
+      ],
+      child: BlocBuilder<ThemeCubit, ThemeMode>(
+        builder: (context, themeMode) {
+          return MaterialApp(
+            title: "Tour De France",
+            debugShowCheckedModeBanner: false,
+            theme: theme.light(),
+            darkTheme: theme.dark(),
+            themeMode: themeMode,
+          );
+        },
+      ),
     );
   }
 }
